5d8f8a1266f2e4b82d2bb18967464d261e85233b
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-10-18 20:15:41 +0500

    fix(com): move CLSID_CoCar definition to iid.h

--- src/components/CarComponent/CarInProcServer.cpp
+++ src/components/CarComponent/CarInProcServer.cpp
@@ -1,13 +1,10 @@
 #include <initguid.h>
 #include <Windows.h>
 #include "CoCarClassFactory.h"
+#include "iid.h"
 
 ULONG _lockCount = 0;
 ULONG _objCount = 0;
-
-// {97B98437-966B-4F72-9527-363603BD8572}
-DEFINE_GUID(CLSID_CoCar,
-	0x97b98437, 0x966b, 0x4f72, 0x95, 0x27, 0x36, 0x36, 0x3, 0xbd, 0x85, 0x72);
 
 STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, void** ppv)
 {
