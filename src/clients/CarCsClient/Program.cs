16a563364446f0e80e0d0f3518b946aab5ec02ce
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-10-21 22:14:54 +0500

    6

--- src/clients/CarCsClient/Program.cs
+++ src/clients/CarCsClient/Program.cs
@@ -13,9 +13,9 @@ namespace CarCsClient
             var carType = Type.GetTypeFromProgID("CarComponent.Car");
             dynamic car = Activator.CreateInstance(carType!);
 
-            var interfaces = carType.GetInterfaces();
-            car.DisplayStats();
-            carType.InvokeMember("SpeedUp", BindingFlags.InvokeMethod, null, car, new object[] { });
+            //var interfaces = carType.GetInterfaces();
+            //car.DisplayStats();
+            //carType.InvokeMember("SpeedUp", BindingFlags.InvokeMethod, null, car, new object[] { });
 
             var iDisp = (IDispatch) car;
             var count = iDisp!.GetTypeInfoCount();
@@ -38,21 +38,21 @@ namespace CarCsClient
         }
     }
 
-    [ComImport, InterfaceType(ComInterfaceType.InterfaceIsDual), Guid("F426AD9B-76F0-46BE-95DE-0B22C84EB89D")]
+    [ComImport, InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("F426AD9B-76F0-46BE-95DE-0B22C84EB89D")]
     public interface ICreateCar
     {
         void SetPetName(string petName);
         void SetMaxSpeed(int maxSp);
     }
 
-    [ComImport, InterfaceType(ComInterfaceType.InterfaceIsDual), Guid("46073856-735C-4D0A-8C30-80BF4ECFC2DC")]
+    [ComImport, InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("46073856-735C-4D0A-8C30-80BF4ECFC2DC")]
     public interface IStats
     {
         void DisplayStats();
         void GetPetName(ref string petName);
     }
 
-    [ComImport, InterfaceType(ComInterfaceType.InterfaceIsDual), Guid("9E37E773-86BD-4DD5-8F74-30536AAFC3B3")]
+    [ComImport, InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("9E37E773-86BD-4DD5-8F74-30536AAFC3B3")]
     public interface IEngine
     {
         void SpeedUp();
@@ -60,7 +60,7 @@ namespace CarCsClient
         void GetCurSpeed(ref int maxSpeed);
     }
 
-    [ComImport, InterfaceType(ComInterfaceType.InterfaceIsDual), Guid("00020400-0000-0000-C000-000000000046")]
+    [ComImport, InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("00020400-0000-0000-C000-000000000046")]
     public interface IDispatch
     {
         int GetTypeInfoCount();
